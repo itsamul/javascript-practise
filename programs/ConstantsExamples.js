e0cc78d3685c41567b61b4d5fb5ee3430cbac369
Amul Sinfal <itsmeamuls@gmail.com>
2025-03-16 16:22:48 +0530

    Added the ConstantsExamples.js file

---
+++ programs/ConstantsExamples.js
@@ -0,0 +1,16 @@
+
+
+var a = 5;
+console.log(a)  // Output: 5 
+
+a = 9; // changing the value of a to 9
+console.log(a)  // Output: 9
+
+const d = 10; // declaring d as constant and assigning value as 10.
+console.log(d); // Output: 10
+
+d = 20;         // When we run the program it will give we will get a TypeError: Assignment to constant variable for line 12.  
+                //  We cannot change the value of a constant. 
+                // It will remain the same for rest of life cycle of the program.
+
+
