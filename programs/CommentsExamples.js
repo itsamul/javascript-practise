5847a29843ca8d29642741459f7a361c1678d708
Amul Sinfal <itsmeamuls@gmail.com>
2025-03-16 16:13:36 +0530

    Added the CommentsExamples.js file

---
+++ programs/CommentsExamples.js
@@ -0,0 +1,15 @@
+//  // is used for Single Line Comment
+//  /* */ is used for Multiple Line Comment
+
+/*
+    This program will add two numbers.
+    We have created two variable a and b.
+    We have initialized both the variables 6 and 10 respectively.
+    line 15  will add and print addition of two variables.
+*/
+
+var a;  // Declaring a variable.
+a = 6;  // initializing the variable.
+var b = 10;
+
+console.log(a + b);
