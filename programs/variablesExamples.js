887438c6fcfb6380492b8627cbc99e71b0e35cd6
Amul Sinfal <itsmeamuls@gmail.com>
2025-03-16 15:19:15 +0530

    Added Javascript Examples

---
+++ programs/variablesExamples.js
@@ -0,0 +1,26 @@
+console.log("Hello world");
+
+var age; //declaring the variable.
+age = 12; //initializing the variable.
+console.log(age); // Number type variable
+
+var userName;
+userName = "Test"  // String type variable
+console.log(userName);
+
+var boolValue;
+boolValue = false; // boolean type variable
+console.log(boolValue);
+
+
+var a = 1;
+console.log(typeof(a))
+
+var b = 1.3;
+console.log(typeof(b))
+
+var c = "String";
+console.log(typeof(c))
+
+var d = true;
+console.log(typeof(d))
